681ce26ae8f10be41edc9d871b8fb54b9c9a852d
repositor01 <122935983+repositor01@users.noreply.github.com>
2023-03-06 19:58:00 -0300

    initial commit

---
+++ CV Carlos Luchesse/cv.js
@@ -0,0 +1,9 @@
+
+document.getElementById ('button').onclick = function () {
+
+            console.log ("Capturamos el evento click");
+
+            document.getElementById("demo").innerHTML =
+                        "Por - hombres entre los artistas y + artistas entre los hombres";
+}
+            
